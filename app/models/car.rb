ff617dcd5f870762551377e84a6e17b48f6cef55
Samy FEREDJ <samyferedj@outlook.fr>
2025-02-26 19:58:36 +0100

    generation of car model, validations and seed

---
+++ app/models/car.rb
@@ -0,0 +1,9 @@
+class Car < ApplicationRecord
+  validates :brand, presence: true
+  validates :car_model, presence: true
+  validates :car_location, presence: true
+  validates :rent_price, presence: true, numericality: true
+  validates :kilometers, numericality: true, allow_nil: true
+  validates :release_year, numericality: { only_integer: true }, allow_nil: true
+  has_one_attached :photo
+end
