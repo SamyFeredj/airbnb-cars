2111393f86890e1c6c75dd228868a48cf58563ae
Samy FEREDJ <samyferedj@outlook.fr>
2025-02-27 19:55:09 +0100

    adding seed for user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 print "Cleaning database..."
 if Rails.env == "development"
   Car.destroy_all
+  User.destroy_all
 end
 puts " OK !"
 
@@ -12,26 +13,54 @@ puts " OK !"
 img_car1 = "https://images.unsplash.com/photo-1542362567-b07e54358753?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 img_car2 = "https://images.unsplash.com/photo-1506610654-064fbba4780c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 img_car3 = "https://images.unsplash.com/photo-1594502225401-a9eab8b405dd?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
+#
+#
+#
+puts " ====================== "
+#
+# ===== USERS
+print "Creating user 1..."
+samy = User.new(email: "samyferedj@outlook.fr", password: "password", first_name: "samy", last_name: "feredj", phone_number: "0123456789")
+samy.save!
+puts " OK !"
+#
+print "Creating user 2..."
+arli = User.new(email: "arli@outlook.fr", password: "password", first_name: "arli", last_name: "sefgjini", phone_number: "1234556789")
+arli.save!
+puts " OK !"
+#
+print "Creating user 3..."
+laurent = User.new(email: "laurent@gmail.com", password: "password", first_name: "laurent", last_name: "millet", phone_number: "3456789123")
+laurent.save!
+puts " OK !"
+#
+print "Creating user 4..."
+marcus = User.new(email: "marcus@yahoo.fr", password: "password", first_name: "marcus", last_name: "meyer", phone_number: "9876543210")
+marcus.save!
+puts " OK !"
+#
+puts " ====================== "
+#
+# ===== CARS
 print "Creating car 1..."
-car1 = Car.new(brand: "Jaguer", car_model: "M4 Hybrid", car_location: "Trocadero - PARIS", rent_price: 200, kilometers: 2_000, release_year: 2025)
+car1 = Car.new(brand: "Jaguer", car_model: "M4 Hybrid", car_location: "Trocadero - PARIS", rent_price: 200, kilometers: 2_000, release_year: 2025, user_id: samy.id)
 car1.photo.attach(io: URI.open(img_car1), filename: 'car1.png', content_type: 'image/png')
 car1.save!
 puts " OK !"
 # ==
 print "Creating car 2..."
-car2 = Car.new(brand: "Ford", car_model: "Mustang", car_location: "Los Angeles - USA", rent_price: 180, kilometers: 25_000, release_year: 2025)
+car2 = Car.new(brand: "Ford", car_model: "Mustang", car_location: "Los Angeles - USA", rent_price: 180, kilometers: 25_000, release_year: 2025, user_id: arli.id)
 car2.photo.attach(io: URI.open(img_car2), filename: 'car2.png', content_type: 'image/png')
 car2.save!
 puts " OK !"
 # ==
 print "Creating car 3..."
-car3 = Car.new(brand: "Renault", car_model: "Capture", car_location: "Angers", rent_price: 50, kilometers: 257_000, release_year: 2024)
+car3 = Car.new(brand: "Renault", car_model: "Capture", car_location: "Angers", rent_price: 50, kilometers: 257_000, release_year: 2024, user_id: samy.id)
 car3.photo.attach(io: URI.open(img_car3), filename: 'car3.png', content_type: 'image/png')
 car3.save!
 puts " OK !"
 
-
 # End of database generation
 puts "End of generation of database"
 puts "#{Car.count} cars generated"
+puts "#{User.count} users generated"
