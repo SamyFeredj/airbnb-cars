297595ed1b48867f49b1d1f60cb5fa1c57d739cf
Arli Sefgjini arlialb@hotmail.com <arlialb@hotmail.com>
2025-02-27 16:08:54 +0100

    Model user debug boutton deconnexion

--- db/migrate/20250227135549_add_not_null_constraint_to_user_id_in_cars.rb
+++ db/migrate/20250227135549_add_not_null_constraint_to_user_id_in_cars.rb
@@ -1,5 +1,15 @@
 class AddNotNullConstraintToUserIdInCars < ActiveRecord::Migration[7.1]
-  def change
+  def up
+    # Assigner un user par défaut aux voitures existantes avant d'appliquer NOT NULL
+    default_user = User.first || User.create!(email: "default@example.com", password: "password", role: "renter")
+
+    Car.where(user_id: nil).update_all(user_id: default_user.id)
+
+    # Appliquer la contrainte NOT NULL une fois la colonne remplie
     change_column_null :cars, :user_id, false
+  end
+
+  def down
+    change_column_null :cars, :user_id, true
   end
 end
