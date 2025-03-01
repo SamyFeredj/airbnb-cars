3638230f2b6f0a0688e1c41d827a2730787187f0
Samy FEREDJ <samyferedj@outlook.fr>
2025-03-01 09:05:37 +0100

    adding #new and #create actions of reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,7 +5,7 @@ class Reservation < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :end_date, comparison: { greater_than: :start_date }
+  validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
   validates :total_price, presence: true, numericality: true
   validates :status, presence: true, inclusion: { in: ["Pending", "Accepted", "Rejected"], allow_nil: false }
 end
