21060da34387cd4ae373cc7dcd1e1f18ee25710e
Samy FEREDJ <samyferedj@outlook.fr>
2025-03-11 17:06:13 +0100

    adding user id

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -14,6 +14,7 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
+    @car.user_id = current_user.id
     if @car.save
       redirect_to @car, notice: "Voiture ajoutée avec succès !"
     else
