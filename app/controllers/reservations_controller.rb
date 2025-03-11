d5cbd9c4daf4bb506688b809f3795f57d51cd86c
Arli Sefgjini arlialb@hotmail.com <arlialb@hotmail.com>
2025-03-11 17:15:45 +0100

    CSS du cars et reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -37,7 +37,6 @@ class ReservationsController < ApplicationController
     end
   end
 
-
   def mycars
     @reservations = Reservation.where(car_id: Car.where(user_id: current_user.id).map { |car| car.id })
   end
